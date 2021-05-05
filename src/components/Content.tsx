8268c67d4607d9095f9e60479b35ddfffdd63ef7
daniel <danielzagocapanema@gmail.com>
2021-05-05 14:42:53 -0300

    Content e Header funcionais

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { Header } from "./Header";
+import { MovieCard } from "./MovieCard";
+
+export interface ContentProps {
+  selectedGenre: {
+    title: string
+  },
+  movies: {
+    imdbID: string,
+    Title: string,
+    Poster: string,
+    Runtime: string,
+    Ratings: Array<{
+      Source: string,
+      Value: string
+      }>;
+  }[]
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      
+      <Header title={props.selectedGenre.title}/>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard 
+                key ={movie.imdbID} 
+                title={movie.Title} 
+                poster={movie.Poster} 
+                runtime={movie.Runtime} 
+                rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
