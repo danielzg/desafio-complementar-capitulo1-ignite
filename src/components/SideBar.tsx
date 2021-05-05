38f2b5efb736daf55b59e1fd40524118745db621
daniel <danielzagocapanema@gmail.com>
2021-05-05 20:18:05 -0300

    Desafio complementar finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SideBarProps {
+  selectedGenreId: number,
+  genres: {
+    id: number,
+    title: string, 
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  }[],
+  click: (id: number) => void
+}
+
+export function SideBar({genres, selectedGenreId, click}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => {
+            click(genre.id)}
+          }
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
